08db9c7f0fa207ce093b93522edc4ac0bf6be471
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-02 00:00:33 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "instance_type"{
+  description = "attaching instance type"
+  type = string
+}
+variable "ami_id"{
+  description = "attaching instance ami-id"
+  type = string 
+}
+variable "key_name"{
+  description = "attaching key_name"
+  type = string 
+}
+variable "vpc_cidr" {
+  description = "Attching cidr for vpc"
+  type = string 
+}
+variable "public_subnet_cidr" {
+  description = "attaching cidr for vpc"
+  type = string
+}
+
+  
