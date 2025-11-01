d3f0bc40bbe012bf97c931d15217713521555d0a
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-01 23:48:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,4 +50,68 @@ resource "aws_route_table_association" "Nginx1_public_asso" {
  route_table_id = aws_route_table.Nginx1_route_table
 }
 
+#creating security group
+
+resource "aws_security_group" "Nginx1_sg" {
+ Name = "Nginx1_sg"
+ description = "allow HTTP and SSH"
+ vpc_id = aws_vpc.Nginx1_vpc
+
+ingress {
+description = "Allow SSH port"
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"]
+}
+
+ingress {
+description = "allow Http port"
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"]
+}
+
+egress {
+description = "allow all outbound"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_block = ["0.0.0.0/0"]
+}
+
+tags = {
+ Name = "Nginx1_sg"
+}
+
+
+#--------------------------------
+#--------------------------------
+
+#creating EC2
+resource "aws_instance" "Nginx1_ec2" {
+ instance_type = var.instance_type
+ ami           = var.ami_id
+ subnet_id     = aws_subnet.Nginx1_subnet
+ key_name      = var.key_name
+ security_groups = [aws.security_group.Nginx1_sg]
+
+ user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              echo "<h1>Welcome to Nginx1!</h1><p>Deployed via Terraform 🚀</p>" > /var/www/html/index.nginx-debian.html
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "Nginx1_ec2"
+  }
+}
+
+
+
+
  
